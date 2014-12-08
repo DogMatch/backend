a7c981a296fd9bace82e5e1e9f0bfa8a5d853d47
bparrish206 <brentparrish@Rosias-MacBook-Pro.local>
2014-12-08 11:39:48 -0800

    set up server

---
+++ gruntfile.js
@@ -0,0 +1,30 @@
+"use strict";
+
+module.exports = function(grunt) {
+  grunt.initConfig({
+    sass: {
+      dist: {
+        files: {
+          'stylesheets/app.css': 'sass/app.sass',
+          'stylesheets/other-app.css': 'sass/app.scss'
+        }
+      }
+    },
+    watch: {
+      sass: {
+        files: ['sass/**/*.sass', 'sass/**/*.scss'],
+        tasks: ['sass']
+      },
+      livereload: {
+        files: ['stylesheets/*.css'],
+        options: {
+          livereload: true
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.registerTask('default', ['sass']);
+};
---
+++ server.js
@@ -0,0 +1,24 @@
+"use strict";
+
+var express = require('express');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var passport = require('passport');
+var app = express();
+
+app.use(bodyParser.json);
+app.use(bodyParser.urlencoded({
+  extended:true
+}));
+app.use(express.static(__dirname + '/public'));
+
+mongoose.connect(process.env.MONGO_URL || 'mongodb://localhost/dogmatch');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {//verifies db is connected
+});
+
+app.set('port', process.env.PORT || 3000);
+app.listen(app.get('port'), function() {
+  console.log("Port is running on port: %d",  app.get('port'));
+});
